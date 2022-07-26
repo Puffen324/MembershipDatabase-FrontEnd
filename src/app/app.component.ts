27021c6731b1620ddf3e98ca0e4ffdf6f7858929
André Söderlund <andre.soderlund@live.com>
2022-07-26 21:29:10 +0200

    Sysslat lite med modals, inget klart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
 import { Member } from './member';
 import { MemberService } from './member.service';
 
@@ -21,4 +21,25 @@ export class AppComponent implements OnInit {
     this.memberService.getMembers().subscribe(members => this.members = members);
       
   }
+
+  public onOpenModal(member: Member, mode: string): void{
+    const table = document.getElementById('main-table')
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if(mode === 'edit'){
+      button.setAttribute('data-target', '#editMemberModal')
+    }
+    if(mode === 'add'){
+      button.setAttribute('data-target', 'addMemberModal')
+    }
+    if(mode === 'delete'){
+      button.setAttribute('data-target', 'deleteMemberModal')
+    }
+    table?.appendChild(button);
+    button.click();
+
+  }
+
 }
